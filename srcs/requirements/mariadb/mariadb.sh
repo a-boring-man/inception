3e3d6eaaac76806fe1e345d005edb163dd537c1a
jonathan <jonathan.rinna@gmail.com>
2023-02-20 11:25:10 +0100

    correction mariadb typo and ssl certificate correction

--- srcs/requirements/mariadb/mariadb.sh
+++ srcs/requirements/mariadb/mariadb.sh
@@ -3,10 +3,10 @@
 service		mysql start
 sleep 5
 mysql -e 	"CREATE DATABASE ${MARIADB_NAME};"
-mysql -e	"CREATE USER '${MARIADB_USR}'@'%' INDENTIFIED BY '${MARIADB_USR_PWD}';"
+mysql -e	"CREATE USER '${MARIADB_USR}'@'%' IDENTIFIED BY '${MARIADB_USR_PWD}';"
 mysql -e	"GRANT ALL PRIVILEGES ON ${MARIADB_NAME}.* TO '${MARIADB_USR}'@'%' IDENTIFIED BY '${MARIADB_USR_PWD}' WITH GRANT OPTION;"
 mysql -e	"FLUSH PRIVILEGES;"
-mysql -e	"ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PWD}'"
 sed -i		"s/password = /password = ${MARIADB_ROOT_PWD} #/" /etc/mysql/debian.cnf
+mysql -e	"ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PWD}'"
 service		mysql stop
 mysqld_safe

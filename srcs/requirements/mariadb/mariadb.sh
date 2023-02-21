5e3e22cd5210ed8dc513f03818e6a226731135c2
jonathan <jonathan.rinna@gmail.com>
2023-02-21 14:18:09 +0100

    correction

--- srcs/requirements/mariadb/mariadb.sh
+++ srcs/requirements/mariadb/mariadb.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-service		mysql start
-sleep 5
-mysql -e 	"CREATE DATABASE ${MARIADB_NAME};"
-mysql -e	"CREATE USER '${MARIADB_USR}'@'%' IDENTIFIED BY '${MARIADB_USR_PWD}';"
-mysql -e	"GRANT ALL PRIVILEGES ON ${MARIADB_NAME}.* TO '${MARIADB_USR}'@'%' IDENTIFIED BY '${MARIADB_USR_PWD}' WITH GRANT OPTION;"
-mysql -e	"FLUSH PRIVILEGES;"
-sed -i		"s/password = /password = ${MARIADB_ROOT_PWD} #/" /etc/mysql/debian.cnf
-mysql -e	"ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PWD}'"
-service		mysql stop
+if [ ! -d "/var/lib/mysql/${MARIADB_NAME}" ]
+then
+
+	service		mysql start
+	sleep 5
+	mysql -e 	"CREATE DATABASE ${MARIADB_NAME};"
+	mysql -e	"CREATE USER '${MARIADB_USR}'@'%' IDENTIFIED BY '${MARIADB_USR_PWD}';"
+	mysql -e	"GRANT ALL PRIVILEGES ON ${MARIADB_NAME}.* TO '${MARIADB_USR}'@'%' IDENTIFIED BY '${MARIADB_USR_PWD}' WITH GRANT OPTION;"
+	mysql -e	"FLUSH PRIVILEGES;"
+	sed -i		"s/password = /password = ${MARIADB_ROOT_PWD} #/" /etc/mysql/debian.cnf
+	mysql -e	"ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PWD}'"
+	service		mysql stop
+
+fi
+
 mysqld_safe
--- srcs/requirements/wordpress/wordpress.sh
+++ srcs/requirements/wordpress/wordpress.sh
@@ -3,6 +3,14 @@
 if [ ! -f "/var/www/wordpress/wp-config.php" ]
 then
 
+	sleep 15
+	while ! mysql -h mariadb --user=${MARIADB_USR} --password=${MARIADB_USR_PWD} -e "SELECT schema_name FROM information_schema.schemata WHERE schema_name='$MARIADB_NAME'"; do
+  		echo "Waiting for database to be created..."
+  		sleep 5
+	done
+
+echo "Starting installation wordpress"
+
 wp core download --allow-root
 wp config create --allow-root --dbname=${MARIADB_NAME} --dbuser=${MARIADB_USR} --dbpass=${MARIADB_USR_PWD} --dbhost=${MARIADB_HOST} --force
 wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PWD} --admin_email=${WP_ADMIN_EMAIL}
